cf4a0401dd52284ccc50a7d1a5af03b2ff74edad
Daniel-Boll <danielboll.academico@gmail.com>
2019-06-27 03:32:06 -0300

    Atualização quest_06

--- quest_06.c
+++ quest_06.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 int main(){
-	int n, A, B, c=1, x=0, countX; //declaração de variaveis inteiras
+	int a, i, n, n_, A, B, c=1, x=0, countX; //declaração de variaveis inteiras
 	float result; //declaracao de variavel float
 	scanf("%d", &n); //le a quantidade de equacões
+    n_ = n;
 	float vetX[n]; //declaração de um vetor de float
-	for(int a=0; a<n;a++) vetX[a]=-1; //seta todos os valores de vetX como -1
+	for(a=0; a<n;a++) vetX[a]=-1; //seta todos os valores de vetX como -1
 	
 	while(n){ //repete o codigo enquanto n seja maior que 0
 		scanf("%d", &B); //le o termo independente 
 		countX=0; //seta countX como 0
 		result=0; //seta result como 0
-		
-		for(int i=0; i<c; i++){ //repete o codigo enquanto i seja menor que c
+		int i;
+		for(i=0; i<c; i++){ //repete o codigo enquanto i seja menor que c
 			scanf("%d", &A); //le os elementos do sistema
 			if (c==1) vetX[x] = B/(float)A; //caso c==0, X1 = termo independente / primeiro elemento da equação
 			
@@ -26,9 +27,12 @@ int main(){
 			countX++; //aumenta 1 ao countX
 		}
 		
-		printf("X%d = %.4f\n", c, vetX[x]);//imprime o resultado do X atual
 		x++; //adiciona 1 ao x
 		c++; //aumenta 1 ao c
 		n--; //decremente o n
 	}
+    for(i=0; i < n_; i++){
+        printf("X%d = %.4f\n", i+1, vetX[i]);//imprime o resultado do X atual
+    }
+    
 }
